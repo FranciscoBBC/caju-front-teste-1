2bbbb0b410c010f7bbb207eb8cd864b6c562fa25
FranciscoBBC <francisco.ruf@gmail.com>
2024-11-21 06:33:32 -0300

    feat: create react query config

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import Router from "~/router";
 import { Header } from "./components/Header";
+import ReactQueryProvider from "./utils/reactQueryProvider";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function App() {
   return (
@@ -7,7 +9,12 @@ function App() {
       <Header>
         <h1>Caju Front Teste</h1>
       </Header>
-      <Router />
+      <ReactQueryProvider>
+        <Router />
+        {process.env.NODE_ENV === "development" && (
+          <ReactQueryDevtools initialIsOpen={false} />
+        )}
+      </ReactQueryProvider>
     </>
   );
 }
